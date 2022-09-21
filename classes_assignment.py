a4f852f8107f3cb616a002790db5d4e725c221a3
Alex Schaffer <alexschaffer@gmail.com>
2022-09-21 19:34:10 -0400

    update

---
+++ classes_assignment.py
@@ -0,0 +1,14 @@
+class Sports:
+    name = 'American Football'
+    country = 'USA'
+    type_of_ball = 'sphere'
+    violence_level ='high'
+
+class Players(Sports):
+    base_pay = '$900,000'
+    competitevness = 'high'
+
+
+class Coaches(Sports):
+    Firing_Frequency: 'high'
+    Diversity: 'very low'
