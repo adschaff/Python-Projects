11e79492d7c7f4cc304a5328da28b73c6ad6a21b
Alex Schaffer <alexschaffer@gmail.com>
2022-09-21 19:58:14 -0400

    Update classes_assignment.py

--- classes_assignment.py
+++ classes_assignment.py
@@ -1,14 +1,16 @@
-class Sports:
+class Sports: #Class Sports created with 4 attributes
     name = 'American Football'
     country = 'USA'
     type_of_ball = 'sphere'
     violence_level ='high'
 
-class Players(Sports):
+class Players(Sports): #Class Players inherists from sports and adds Players to the class with its own attributes
     base_pay = '$900,000'
-    competitevness = 'high'
+    competiteveness = 'high'
 
 
-class Coaches(Sports):
+class Coaches(Sports): #Class Coaches inherists from sports and adds Players to the class with its own attributes
     Firing_Frequency: 'high'
     Diversity: 'very low'
+
+
